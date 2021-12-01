1cb0b616ea021f1de6614f121ed06553f7cce911
unknown <ulicesvillegas7@gmail.com>
2021-11-30 18:53:51 -0700

    clase 13 oop revision

--- Clase13OOPjs/app.js
+++ Clase13OOPjs/app.js
@@ -63,9 +63,18 @@ console.log(Person1.staticTest('chido')) //NO INSTANTIATION
 //INHERITANCE
 
 class Student extends Person1{
-    constructor(name, age, grade){
-        super(name, age)
-        this.grade = grade
+    constructor(name, age, grade = []){
+        super(name, age);
+        this.grade = grade;
+       // this.grade = grade || [];
+    }
+
+    set setGrade(grades){
+        this.grade.push(grades)
+    }
+
+    get getGrade(){
+        return this.grade
     }
     sayHi(){
         return `${this.name} is ${this.age} yrs old. Message from Student`;//Polymorfismo
@@ -75,3 +84,29 @@ class Student extends Person1{
 const julie = new Student("Julie", 22,"A");
 console.log(julie);
 console.log(julie.sayHi());
+const marcus = new Student("Marcus", 20)
+//const marcus = new Student("Marcus", 20, "B");
+marcus.setGrade = 'D';
+marcus.setGrade = 'F';
+marcus.setGrade = 'C';
+console.log(marcus.getGrade);
+console.log(marcus)
+
+//PRIVATE CLASS
+ class Estudiantes extends Person1{
+     #grades = [];
+     set setGrade(grade){
+         this.#grades.push(grade);
+    }
+
+    get getGrade(){
+        return this.#grades
+    }
+};
+
+const paty = new Estudiantes("Paty", 25);
+paty.setGrade = 'B';
+paty.setGrade = 'A';
+paty.setGrade = 'C';
+console.log(paty.getGrade)
+console.log(paty)
